75360dc9bd06c7b9e15291740d92b108ae1e5aa9
Bruno Alla <browniebroke@users.noreply.github.com>
2024-03-29 10:41:11 +0000

    chore: convert commitlint config syntax to ESM

--- commitlint.config.mjs
+++ commitlint.config.mjs
@@ -1,4 +1,4 @@
-module.exports = {
+const Configuration = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "header-max-length": [0, "always", Infinity],
@@ -6,3 +6,5 @@ module.exports = {
     "footer-max-line-length": [0, "always", Infinity],
   },
 };
+
+export default Configuration;
